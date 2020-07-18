79361f8b5019dba27dc4a46264415d917bd61501
MikeChunko <chunkomike@gmail.com>
2020-07-18 15:16:40 -0400

    Math implementation

--- raytrace.cpp
+++ raytrace.cpp
@@ -60,11 +60,11 @@ ostream& operator<<(ostream& os, Ray const& r) {
 // Represent a sphere
 // Used in scene creation
 struct Sphere {
-    Vec center;
+    Vec center, color;
     double radius;
 
-    Sphere() { center = Vec(); radius = 0; }
-    Sphere(const Vec& c, double r) : center(c), radius(r) {}
+    Sphere() { center = Vec(); color = Vec(); radius = 0; }
+    Sphere(const Vec& c, const Vec& col, double r) : center(c), color(col), radius(r) {}
 
     bool intersect(const Ray& ray, double &t) const {
         const Vec oc = ray.origin - center;
@@ -96,38 +96,37 @@ void color_clamp(Vec& v) {
 
 
 int main() {
-    const int HEIGHT = 20, WIDTH = 20;  // Image height and width
+    const int HEIGHT = 200, WIDTH = 200;  // Image height and width
     const Vec white(255,255,255), // Quick reference colors
               black(0,0,0),
               green(0,255,0);
 
+    // Header for .ppm
     ofstream out = ofstream("result.ppm");
     out << "P3\n" << WIDTH << ' ' << HEIGHT << " 255\n";
 
     // Scene creation
-    const Sphere sphere(Vec(.5*WIDTH, .5*HEIGHT, 50), .25*WIDTH),  // Sphere centered and with diameter half as wide as the screen
-                 light(Vec(0,0,50), 1);  // Point light source at the same depth as the sphere
+    const Sphere sphere(Vec(.5*WIDTH, .5*HEIGHT, 50), green, .5*WIDTH),  // Sphere centered and with diameter half as wide as the screen
+                 light(Vec(1.5*WIDTH,1.5*HEIGHT, 50), white, 1);  // Point light source at the same depth as the sphere
 
 	double t;
 	Vec color;
 
     for (int y = 0; y < HEIGHT; y++) {
         for (int x = 0; x < WIDTH; x++) {
-            color = Vec(0,0,0);  // Initialize to black (nothing)
+            color = black;  // Initialize to black (nothing)
             const Ray ray(Vec(x,y,0), Vec(0,0,1));  // Initial ray
 
-            if (sphere.intersect(ray, t)) { // Do some funky math (not sure on how this works)
+            if (sphere.intersect(ray, t)) {  // Do some funky math (not sure on how this works)
 				const Vec p = ray.origin + ray.direction*t,
 						  l = light.center - p,
 						  n = sphere.normal(p);
-				// TODO: determine pixel color from these
+				const double dt = dot(l.normalize(), n.normalize());
+
+                color = (sphere.color + light.color*dt) * .5;
 			}
 
-            // ! Debug
-            cout << color;
-            if (x < WIDTH - 1)
-                cout << ", ";
-
+            // Output color at current pixel
             color_clamp(color);
             out << (int)color.x << ' ' << (int)color.y << ' ' << (int)color.z << "\n";
         }
